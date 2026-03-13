0127c51c6beae7b24bd625677b71a8d0e25946ad
BugTerminator07 <republicofgamerssakin@gmail.com>
2026-03-13 23:00:58 +0600

    first commit

---
+++ payoo-resources/script/login.js
@@ -0,0 +1,23 @@
+document.getElementById('login-btn').addEventListener("click",
+    function(){
+        // get the mobile number
+        const numberInput=document.getElementById('input-number');
+        const contactNumber=numberInput.value;
+
+        // get the pin
+        const inputPin=document.getElementById('input-pin');
+        const pin=inputPin.value;
+
+        // match the mobile number and pin
+        if(contactNumber=="01730633804" && pin=="1234"){
+         // if true:::alert>homepage
+         alert('Login Successful');
+         window.location.assign("/home.html");
+        }
+        // if false:::alert>return
+        else{
+            alert("Log-In Failed");
+            return;
+        }
+    }
+)
